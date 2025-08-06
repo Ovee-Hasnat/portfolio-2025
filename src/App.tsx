31995322bcd7013273252d77e0da0387b678fc1c
Ovee Hasnat <abu.hasnat@northsouth.edu>
2025-08-06 14:18:15 +0600

    fix vercel issues

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { ReactLenis } from "lenis/react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./route/Router";
 import { Toaster } from "./components/ui/toaster";
-import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
