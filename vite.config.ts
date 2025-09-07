84ca44ee87906cca1e49360928702d189d36eceb
Ovee Hasnat <abu.hasnat@northsouth.edu>
2025-09-07 13:36:48 +0600

    performance optimization 1.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ open: true }), // opens a visual report after build
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
